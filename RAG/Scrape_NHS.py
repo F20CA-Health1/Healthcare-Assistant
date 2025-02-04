636952f972c820ec5b399d0eedd7150045c224c2
Kareem Shadi Basem Al-Hasan <kareemshadi@icloud.com>
2025-02-04 11:19:00 +0000

    Added script to scrape nhs to get all illnesses and its link

---
+++ RAG/Scrape_NHS.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://www.nhsinform.scot/illnesses-and-conditions/a-to-z/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+illnesses = soup.find_all('div', class_="az_list_indivisual")
+
+dictionary = {}
+for i in range(len(illnesses)):
+    letter = illnesses[i]
+    ls = []
+    for j in range(len(letter.find_all('a'))):
+        ls.append((letter.find_all('a')[j].text, letter.find_all('a')[j].get('href')))
+    dictionary[letter.find_all('h2')[0].text] = ls
+
+# write the dictionary to a json file
+with open('RAG/nhs_illnesses.json', 'w') as f:
+    json.dump(dictionary, f)
